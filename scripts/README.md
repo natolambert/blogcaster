6122a38a611642cd1ad323c7d52e372343eea935
Nathan Lambert <nathan@huggingface.co>
2023-12-28 16:26:52 -0500

    clean

--- scripts/README.md
+++ scripts/README.md
@@ -1,6 +1,6 @@
 # Scripts
 
-There are two sections of scripts, the core ones and the "in development" section.
+There are two sections of scripts, the core ones and the "experimental" section.
 The core scripts are as follows:
 1. `create-config.py`: Parses markdown into a `yml` file.
 2. `tts.py`: Queries 11labs to create audio. Merges files and prints chapters.
--- scripts/ttv-generate.py
+++ scripts/ttv-generate.py
@@ -28,21 +28,6 @@ def strip_title(string):
         return string
 
 
-# TODO investigate below
-"""
-Exception in thread Thread-3 (_handle_results):
-Traceback (most recent call last):
-  File "/Users/nato/miniconda3/envs/media/lib/python3.10/threading.py", line 1009, in _bootstrap_inner
-    self.run()
-  File "/Users/nato/miniconda3/envs/media/lib/python3.10/threading.py", line 946, in run
-    self._target(*self._args, **self._kwargs)
-  File "/Users/nato/miniconda3/envs/media/lib/python3.10/multiprocessing/pool.py", line 576, in _handle_results
-    task = get()
-  File "/Users/nato/miniconda3/envs/media/lib/python3.10/multiprocessing/connection.py", line 256, in recv
-    return _ForkingPickler.loads(buf.getbuffer())
-TypeError: APIStatusError.__init__() missing 2 required keyword-only arguments: 'response' and 'body'
-"""
-
 
 def get_image(idx, string):
     # when figures exist, do not generate (string is None)
